c1f61991c1a3d79b8643418d4885ac3f7f054488
Yissu <semtax@gmx.de>
2013-02-20 14:13:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Aktionhaus Vices2</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css" />
+    </head>
+    <body>
+        <div class="itembox">
+            Lorem Ipsum
+            Lorem Ipsum
+            Lorem Ipsum
+        </div>
+    </body>
+</html>
